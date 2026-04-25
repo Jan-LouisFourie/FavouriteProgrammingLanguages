b6b6c9396fe2e99e11f27473a349e4e85a22be30
Jan-Louis <20231441@ctucareer.co.za>
2026-04-25 11:41:30 +0200

    Add project files.

---
+++ FavouriteProgrammingLanguages/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FavouriteProgrammingLanguages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddBTN = new System.Windows.Forms.Button();
+            this.RemoveBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Favourite Programming Languages";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(832, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(57, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(832, 212);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // AddBTN
+            // 
+            this.AddBTN.Location = new System.Drawing.Point(57, 429);
+            this.AddBTN.Name = "AddBTN";
+            this.AddBTN.Size = new System.Drawing.Size(135, 33);
+            this.AddBTN.TabIndex = 3;
+            this.AddBTN.Text = "Add Language";
+            this.AddBTN.UseVisualStyleBackColor = true;
+            this.AddBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RemoveBTN
+            // 
+            this.RemoveBTN.Location = new System.Drawing.Point(222, 429);
+            this.RemoveBTN.Name = "RemoveBTN";
+            this.RemoveBTN.Size = new System.Drawing.Size(135, 33);
+            this.RemoveBTN.TabIndex = 4;
+            this.RemoveBTN.Text = "Remove Language";
+            this.RemoveBTN.UseVisualStyleBackColor = true;
+            this.RemoveBTN.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 476);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(946, 533);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RemoveBTN);
+            this.Controls.Add(this.AddBTN);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AddBTN;
+        private System.Windows.Forms.Button RemoveBTN;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FavouriteProgrammingLanguages/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FavouriteProgrammingLanguages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Please enter a language name.");
+                return;
+            }
+
+            if (listBox1.Items.Contains(input))
+            {
+                MessageBox.Show("That language is already in the list!");
+            }
+            else
+            {
+                listBox1.Items.Add(input);
+                label2.Text = $"Added '{input}' at {DateTime.Now:dd MMM yyyy HH:mm:ss}";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string removedLanguage = listBox1.SelectedItem.ToString();
+                listBox1.Items.Remove(removedLanguage);
+
+                label2.Text = $"Removed '{removedLanguage}' at {DateTime.Now:dd MMM yyyy HH:mm:ss}";
+            }
+            else
+            {
+                MessageBox.Show("Please select a language from the list to remove.");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
